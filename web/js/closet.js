02da340ad982103b2613a2d863f5a7ac27042978
nosey-dewdrop <damummyphus@gmail.com>
2026-07-23 21:42:58 +0300

    yoke split canli: doll dress yatay yoke dikisi

--- web/js/closet.js
+++ web/js/closet.js
@@ -1,8 +1,8 @@
 // Closet: saved patterns, open inline, delete, or start a new one.
-import { applyStatic, t } from './i18n.js?v=104';
-import { printPattern } from './print.js?v=104';
-import { renderResult } from './render.js?v=104';
-import { loadCloset, deleteFromCloset } from './store.js?v=104';
+import { applyStatic, t } from './i18n.js?v=105';
+import { printPattern } from './print.js?v=105';
+import { renderResult } from './render.js?v=105';
+import { loadCloset, deleteFromCloset } from './store.js?v=105';
 
 const screen = document.getElementById('screen');
 
